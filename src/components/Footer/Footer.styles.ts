022f80e415005b544443b2efa9d3b69eb8f8645a
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-07 22:50:19 +0500

    Added

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -23,4 +23,9 @@ const Right = styled.div``;
 const FooterMiddle = styled.div``;
 const FooterLower = styled.div``;
 
+const MiddleLeft =styled.div``;
+
+const MdiileRight = styled.div`
+`
+
 export { FooterSection, FooterUpper, Left, Right, FooterMiddle };
