7c8f7e3f7841a0963743dec57153743f9d7bc3be
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-07 23:02:04 +0500

    Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import { Home } from "./pages";
-import { createBrowserRouter, CreateBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  CreateBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([{ path: "/", element: <Home /> },
+  {path:}
+]);
 
-const router = createBrowserRouter([
-  {}
-])
-
-function App() {
-  return <Home />;
-}
+function App() {}
 
 export default App;
