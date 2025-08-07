b1ea4b776b695c39b91302bb2d4e180ffefc0c3b
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-07 22:37:04 +0500

    Git Footer sections adde

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -4,3 +4,10 @@ const FooterSection = styled.div`
   height: 100vh;
   width: 100vw;
 `;
+
+const FooterUpper = styled.div`
+  width: 50%;
+  height: 200px;
+`;
+
+export { FooterSection, FooterUpper };
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { FooterSection, FooterUpper } from "./Footer.styles";
 const Footer = () => {
   return (
     <FooterSection>
