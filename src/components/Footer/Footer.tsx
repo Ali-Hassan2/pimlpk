22fd8e570748c277e3834e9eda76542e0d60b816
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-07 22:41:07 +0500

    Git Footer sections adde

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { FooterSection, FooterUpper } from "./Footer.styles";
+import { FooterSection, FooterUpper, Left, Right } from "./Footer.styles";
 const Footer = () => {
   return (
     <FooterSection>
       <FooterUpper>
-        <Left>
-
-        </Left>
-        <Right>
-
-        </Right>
+        <Left></Left>
+        <Right></Right>
       </FooterUpper>
     </FooterSection>
   );
