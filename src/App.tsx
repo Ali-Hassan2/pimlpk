a426c18eba877ae0b7795c594e7f802b355d4fcb
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-07 23:14:42 +0500

    Added

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/services", element: <Services /> },
   {path:'/contactus', element:<ContactUs/>}
-  {path:'aboutus'}
+  {path:'aboutus', element:<AboutUs/>}
   { path: "*", element: <NotFound /> },
 ]);
 
