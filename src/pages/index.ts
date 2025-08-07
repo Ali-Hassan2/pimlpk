1d8048093278735b5e8a4a019456c979b1502487
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-07 23:11:57 +0500

    Added

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { Home } from "./Home/Home";
 export { Services } from "./Services/Services";
 export { NotFound } from "./Notfound/NotFound";
 export { AboutUs } from "./About Us/AboutUs";
+export { ContactUs } from "./ContactUs/ContactUs";
