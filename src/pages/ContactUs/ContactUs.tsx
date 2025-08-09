a7c20751a27f9dc6f1ced5296a6ed671ce061ac9
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-09 17:35:22 +0500

    working on the contact us

--- src/pages/ContactUs/ContactUs.tsx
+++ src/pages/ContactUs/ContactUs.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Navbar } from "../../components";
+
+const NavbarProps = {
+  title: "pimlpk",
+  spant: "Travels",
+  An1: "Home",
+  An2: "Services",
+  An3: "About Us",
+  An4: "Contact Us",
+  email: "ali@example.com",
+  phone: "03212385555",
+};
+
+interface formtypes {
+  name: string;
+  email: string;
+  city: string;
+  message: string;
+}
 
 const ContactUs = () => {
-  return <div>THis is the Contact us</div>;
+  const [formdata, setformdata] = useState<formtypes>({
+    name: "",
+    email: "",
+    city: "",
+    message: "",
+  });
+  return (
+    <>
+      <Navbar {...NavbarProps} />
+    </>
+  );
 };
 
 export { ContactUs };
